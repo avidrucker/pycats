c8c915444e3064dac24c4bc0f182287c0829b9a7
avidrucker <avidrucker@gmail.com>
2025-06-26 13:52:49 -0400

    update todos

--- pycats/core/input.py
+++ pycats/core/input.py
@@ -2,13 +2,15 @@
 from dataclasses import dataclass
 import pygame as pg  # type: ignore
 
+#### TODO: research how key buffering is typically done for fighting games
+#### TODO: implement buffered keys for smash attacks, consecutive regular attacks that require fresh presses, and directional inputs for attacks (e.g. up, down, left, right) that can be buffered for a short time
 
 @dataclass
 class InputFrame:
     held: set[int]  # keys held down this frame
     pressed: set[int]  # keys that went down THIS frame, i.e. "just pressed"
     released: set[int]  # keys that were up THIS frame, i.e. "just released"
-
+    # buffered: set[int]   # keys pressed in the last N frames
 
 _currently_held: set[int] = set()
 
